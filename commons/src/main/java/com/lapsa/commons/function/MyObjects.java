2d11a286eae374552f76f1c157b76257364fa140
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-10-04 17:53:14 +0600

    isa isnull and negates and tests

--- commons/src/main/java/com/lapsa/commons/function/MyObjects.java
+++ commons/src/main/java/com/lapsa/commons/function/MyObjects.java
@@ -7,9 +7,74 @@ public final class MyObjects {
     private MyObjects() {
     }
 
-    public static final boolean isA(Object x, Class<?> clazz) {
-	return clazz.isAssignableFrom(x.getClass());
+    //
+
+    public static <T> boolean nonNull(T obj) {
+	return obj != null;
     }
+
+    public static <T> T requireNonNull(T obj) {
+	return requireNonNull(obj, "is a null object");
+    }
+
+    public static <T> T requireNonNull(T obj, String message) {
+	if (nonNull(obj))
+	    return obj;
+	throw new IllegalArgumentException(message);
+    }
+
+    //
+
+    public static <T> boolean isNull(T obj) {
+	return obj == null;
+    }
+
+    public static <T> T requireNull(T obj) {
+	return requireNull(obj, "is a non-null object");
+    }
+
+    public static <T> T requireNull(T obj, String message) {
+	if (isNull(obj))
+	    return obj;
+	throw new IllegalArgumentException(message);
+    }
+
+    //
+
+    public static final boolean isA(Object x, Class<?> clazz) {
+	requireNonNull(clazz);
+	return x != null && clazz.isAssignableFrom(x.getClass());
+    }
+
+    public static final <T> T requireA(T x, Class<?> clazz) {
+	requireNonNull(clazz);
+	return requireA(x, clazz, "is not a " + clazz.getName());
+    }
+
+    public static final <T> T requireA(T x, Class<?> clazz, String message) {
+	if (isA(x, clazz))
+	    return x;
+	throw new IllegalArgumentException(message);
+    }
+
+    //
+
+    public static final boolean isNotA(Object x, Class<?> clazz) {
+	return !isA(x, clazz);
+    }
+
+    public static final <T> T requireNotA(T x, Class<?> clazz) {
+	requireNonNull(clazz);
+	return requireNotA(x, clazz, "is a " + clazz.getName());
+    }
+
+    public static final <T> T requireNotA(T x, Class<?> clazz, String message) {
+	if (isNotA(x, clazz))
+	    return x;
+	throw new IllegalArgumentException(message);
+    }
+
+    //
 
     public static final <T> Function<Object, T> cast(Class<T> clazz) {
 	return clazz::cast;
---
+++ commons/src/test/java/test/function/MyObjectsTest.java
@@ -0,0 +1,62 @@
+package test.function;
+
+import static org.junit.Assert.*;
+import static test.function.Expeptions.*;
+
+import org.junit.Test;
+
+import com.lapsa.commons.function.MyObjects;
+
+public class MyObjectsTest {
+
+    @Test
+    public void test_null() {
+
+	final Object NULL = null;
+	final Object NON_NULL = new Object();
+
+	assertTrue(MyObjects.isNull(NULL));
+	assertTrue(MyObjects.nonNull(NON_NULL));
+	unexpectException(() -> MyObjects.requireNull(NULL));
+	unexpectException(() -> MyObjects.requireNull(NULL, ""));
+	unexpectException(() -> MyObjects.requireNonNull(NON_NULL));
+	unexpectException(() -> MyObjects.requireNonNull(NON_NULL, ""));
+
+	assertFalse(MyObjects.nonNull(NULL));
+	assertFalse(MyObjects.isNull(NON_NULL));
+	expectException(() -> MyObjects.requireNonNull(NULL), IllegalArgumentException.class);
+	expectException(() -> MyObjects.requireNonNull(NULL, ""), IllegalArgumentException.class);
+	expectException(() -> MyObjects.requireNull(NON_NULL), IllegalArgumentException.class);
+	expectException(() -> MyObjects.requireNull(NON_NULL, ""), IllegalArgumentException.class);
+    }
+
+    @Test
+    public void test_isA() {
+
+	final Class<?> CLAZZ = CharSequence.class;
+
+	final String IS_A = "";
+	final Integer IS_NOT_A = new Integer(1);
+	final String NULL = null;
+
+	assertTrue(MyObjects.isA(IS_A, CLAZZ));
+	assertTrue(MyObjects.isNotA(IS_NOT_A, CLAZZ));
+	assertTrue(MyObjects.isNotA(NULL, CLAZZ));
+	unexpectException(() -> MyObjects.requireA(IS_A, CLAZZ));
+	unexpectException(() -> MyObjects.requireA(IS_A, CLAZZ, ""));
+	unexpectException(() -> MyObjects.requireNotA(IS_NOT_A, CLAZZ));
+	unexpectException(() -> MyObjects.requireNotA(IS_NOT_A, CLAZZ, ""));
+	unexpectException(() -> MyObjects.requireNotA(NULL, CLAZZ));
+	unexpectException(() -> MyObjects.requireNotA(NULL, CLAZZ, ""));
+
+	assertFalse(MyObjects.isNotA(IS_A, CLAZZ));
+	assertFalse(MyObjects.isA(IS_NOT_A, CLAZZ));
+	assertFalse(MyObjects.isA(NULL, CLAZZ));
+	expectException(() -> MyObjects.requireNotA(IS_A, CLAZZ), IllegalArgumentException.class);
+	expectException(() -> MyObjects.requireNotA(IS_A, CLAZZ, ""), IllegalArgumentException.class);
+	expectException(() -> MyObjects.requireA(IS_NOT_A, CLAZZ), IllegalArgumentException.class);
+	expectException(() -> MyObjects.requireA(IS_NOT_A, CLAZZ, ""), IllegalArgumentException.class);
+	expectException(() -> MyObjects.requireA(NULL, CLAZZ), IllegalArgumentException.class);
+	expectException(() -> MyObjects.requireA(NULL, CLAZZ, ""), IllegalArgumentException.class);
+    }
+}
