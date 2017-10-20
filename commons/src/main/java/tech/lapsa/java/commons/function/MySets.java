cc94564f966e7e648a10eeae451c223f16ea4d59
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-10-20 10:51:08 +0600

    mysets

---
+++ commons/src/main/java/tech/lapsa/java/commons/function/MySets.java
@@ -0,0 +1,17 @@
+package tech.lapsa.java.commons.function;
+
+import java.util.Set;
+import java.util.stream.Stream;
+
+public final class MySets {
+
+    private MySets() {
+    }
+
+    @SafeVarargs
+    public static <E> Set<E> of(E... elements) {
+	return Stream.of(elements) //
+		.collect(MyCollectors.unmodifiableSet());
+    }
+
+}
