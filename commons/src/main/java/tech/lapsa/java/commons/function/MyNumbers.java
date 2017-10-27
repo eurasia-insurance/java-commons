92774e667fe50a0a1f47cf29ed1728dc4c53c00e
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-10-27 09:44:37 +0600

    Optional parsers

--- commons/src/main/java/tech/lapsa/java/commons/function/MyNumbers.java
+++ commons/src/main/java/tech/lapsa/java/commons/function/MyNumbers.java
@@ -1,5 +1,10 @@
 package tech.lapsa.java.commons.function;
 
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.OptionalLong;
+
 public final class MyNumbers {
 
     private static final String NON_ZERO_NUMBER = "Is not zero";
@@ -142,4 +147,188 @@ public final class MyNumbers {
 	return n1 != null && n2 != null && n1.equals(n2);
     }
 
+    // parsers
+
+    // int
+
+    // primitive
+
+    public static OptionalInt parseInt(String s) {
+	try {
+	    return OptionalInt.of(Integer.parseInt(s));
+	} catch (NumberFormatException e) {
+	    return OptionalInt.empty();
+	}
+    }
+
+    public static OptionalInt parseNonZeroInt(String s) {
+	try {
+	    return OptionalInt.of(requireNonZero(Integer.parseInt(s)));
+	} catch (IllegalArgumentException e) {
+	    // also catches NumberFormatException
+	    return OptionalInt.empty();
+	}
+    }
+
+    public static OptionalInt parseInt(String s, int radix) {
+	try {
+	    return OptionalInt.of(Integer.parseInt(s, radix));
+	} catch (NumberFormatException e) {
+	    return OptionalInt.empty();
+	}
+    }
+
+    public static OptionalInt parseNonZeroInt(String s, int radix) {
+	try {
+	    return OptionalInt.of(requireNonZero(Integer.parseInt(s, radix)));
+	} catch (IllegalArgumentException e) {
+	    // also catches NumberFormatException
+	    return OptionalInt.empty();
+	}
+    }
+
+    // boxed
+
+    public static Optional<Integer> valueOfInt(String s) {
+	try {
+	    return Optional.of(Integer.valueOf(s));
+	} catch (NumberFormatException e) {
+	    return Optional.empty();
+	}
+    }
+
+    public static Optional<Integer> nonZeroValueOfInt(String s) {
+	try {
+	    return MyOptionals.of(Integer.valueOf(s));
+	} catch (NumberFormatException e) {
+	    return Optional.empty();
+	}
+    }
+
+    public static Optional<Integer> valueOfInt(String s, int radix) {
+	try {
+	    return Optional.of(Integer.valueOf(s, radix));
+	} catch (NumberFormatException e) {
+	    return Optional.empty();
+	}
+    }
+
+    public static Optional<Integer> nonZeroValueOfInt(String s, int radix) {
+	try {
+	    return MyOptionals.of(Integer.valueOf(s, radix));
+	} catch (NumberFormatException e) {
+	    return Optional.empty();
+	}
+    }
+
+    // double
+
+    // primitive
+
+    public static OptionalDouble parseDbl(String s) {
+	try {
+	    return OptionalDouble.of(Double.parseDouble(s));
+	} catch (NumberFormatException e) {
+	    return OptionalDouble.empty();
+	}
+    }
+
+    public static OptionalDouble parseNonZeroDbl(String s) {
+	try {
+	    return OptionalDouble.of(requireNonZero(Double.parseDouble(s)));
+	} catch (IllegalArgumentException e) {
+	    // also catches NumberFormatException
+	    return OptionalDouble.empty();
+	}
+    }
+
+    // boxed
+
+    public static Optional<Double> valueOfDbl(String doubleStr) {
+	try {
+	    return Optional.of(Double.valueOf(doubleStr));
+	} catch (NumberFormatException e) {
+	    return Optional.empty();
+	}
+    }
+
+    public static Optional<Double> nonZeroValueOfDbl(String s) {
+	try {
+	    return MyOptionals.of(Double.valueOf(s));
+	} catch (NumberFormatException e) {
+	    return Optional.empty();
+	}
+    }
+
+    // long
+
+    // primitive
+
+    public static OptionalLong parseLng(String s) {
+	try {
+	    return OptionalLong.of(Long.parseLong(s));
+	} catch (NumberFormatException e) {
+	    return OptionalLong.empty();
+	}
+    }
+
+    public static OptionalLong parseNonZeroLng(String s) {
+	try {
+	    return OptionalLong.of(requireNonZero(Long.parseLong(s)));
+	} catch (IllegalArgumentException e) {
+	    // also catches NumberFormatException
+	    return OptionalLong.empty();
+	}
+    }
+
+    public static OptionalLong parseLng(String s, int radix) {
+	try {
+	    return OptionalLong.of(Long.parseLong(s, radix));
+	} catch (NumberFormatException e) {
+	    return OptionalLong.empty();
+	}
+    }
+
+    public static OptionalLong parseNonZeroLng(String s, int radix) {
+	try {
+	    return OptionalLong.of(requireNonZero(Long.parseLong(s, radix)));
+	} catch (IllegalArgumentException e) {
+	    // also catches NumberFormatException
+	    return OptionalLong.empty();
+	}
+    }
+
+    // boxed
+
+    public static Optional<Long> valueOfLng(String s) {
+	try {
+	    return Optional.of(Long.valueOf(s));
+	} catch (NumberFormatException e) {
+	    return Optional.empty();
+	}
+    }
+
+    public static Optional<Long> nonZeroValueOfLng(String s) {
+	try {
+	    return MyOptionals.of(Long.valueOf(s));
+	} catch (NumberFormatException e) {
+	    return Optional.empty();
+	}
+    }
+
+    public static Optional<Long> valueOfLng(String s, int radix) {
+	try {
+	    return Optional.of(Long.valueOf(s, radix));
+	} catch (NumberFormatException e) {
+	    return Optional.empty();
+	}
+    }
+
+    public static Optional<Long> nonZeroValueOfLng(String s, int radix) {
+	try {
+	    return MyOptionals.of(Long.valueOf(s, radix));
+	} catch (NumberFormatException e) {
+	    return Optional.empty();
+	}
+    }
 }
