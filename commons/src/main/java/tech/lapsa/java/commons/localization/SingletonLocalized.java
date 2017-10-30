718730856a565005598976295e5af62edf355bdd
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-10-30 16:01:51 +0600

    Localized singleton

---
+++ commons/src/main/java/tech/lapsa/java/commons/localization/SingletonLocalized.java
@@ -0,0 +1,21 @@
+package tech.lapsa.java.commons.localization;
+
+import java.util.Locale;
+
+import tech.lapsa.java.commons.function.MyStrings;
+
+public class SingletonLocalized implements Localized {
+
+    private static final long serialVersionUID = 1L;
+
+    public final String string;
+
+    public SingletonLocalized(String string) {
+	this.string = MyStrings.requireNonEmpty(string, "string");
+    }
+
+    @Override
+    public String localized(LocalizationVariant variant, Locale locale) {
+	return string;
+    }
+}
