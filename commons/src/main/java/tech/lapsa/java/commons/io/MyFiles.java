30bcdac33a7c2d9d64635b72c6bf1f2ab4da81cb
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-11-14 10:24:39 +0600

    MyFiles and TODO

---
+++ commons/src/main/java/tech/lapsa/java/commons/io/MyFiles.java
@@ -0,0 +1,28 @@
+package tech.lapsa.java.commons.io;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Optional;
+
+import tech.lapsa.java.commons.function.MyOptionals;
+import tech.lapsa.java.commons.function.MyStrings;
+
+public final class MyFiles {
+
+    private MyFiles() {
+    }
+
+    public static Optional<InputStream> optionalAsStream(final String file) {
+	return MyOptionals.of(getAsStream(file));
+    }
+
+    public static InputStream getAsStream(final String file) {
+	MyStrings.requireNonEmpty(file, "file");
+	try {
+	    return new FileInputStream(file);
+	} catch (FileNotFoundException e) {
+	    return null;
+	}
+    }
+}
