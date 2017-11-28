77a668b28d9ca87bd3dc46edf69203803d648b48
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-11-28 11:24:30 +0600

    annotations finding on superclasses too

--- commons/src/main/java/tech/lapsa/java/commons/reflect/MyAnnotations.java
+++ commons/src/main/java/tech/lapsa/java/commons/reflect/MyAnnotations.java
@@ -3,12 +3,26 @@ package tech.lapsa.java.commons.reflect;
 import java.lang.annotation.Annotation;
 import java.util.Optional;
 
+import tech.lapsa.java.commons.function.MyObjects;
 import tech.lapsa.java.commons.function.MyOptionals;
 
 public class MyAnnotations {
 
-    public static <T extends Annotation> Optional<T> optionalOf(Class<?> clazz, Class<T> annotationClazz) {
-	return MyOptionals.of(clazz.getAnnotation(annotationClazz));
+    public static <T extends Annotation> Optional<T> optionalOf(final Class<?> clazz, final Class<T> annotationClazz) {
+	return MyOptionals.of(of(clazz, annotationClazz));
+    }
+
+    public static <T extends Annotation> T of(final Class<?> clazz, final Class<T> annotationClazz) {
+	MyObjects.requireNonNull(clazz, "clazz");
+	MyObjects.requireNonNull(annotationClazz, "annotationClazz");
+	Class<?> nextCurrent = clazz;
+	do {
+	    final T an = nextCurrent.getAnnotation(annotationClazz);
+	    if (an != null)
+		return an;
+	    nextCurrent = nextCurrent.getSuperclass();
+	} while (nextCurrent != null);
+	return null;
     }
 
 }
