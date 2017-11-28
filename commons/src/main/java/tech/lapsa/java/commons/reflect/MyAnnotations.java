7f0bd61cb51e91c600599c51e559154d7b079398
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-11-28 11:32:33 +0600

    of supers

--- commons/src/main/java/tech/lapsa/java/commons/reflect/MyAnnotations.java
+++ commons/src/main/java/tech/lapsa/java/commons/reflect/MyAnnotations.java
@@ -8,21 +8,30 @@ import tech.lapsa.java.commons.function.MyOptionals;
 
 public class MyAnnotations {
 
-    public static <T extends Annotation> Optional<T> optionalOf(final Class<?> clazz, final Class<T> annotationClazz) {
-	return MyOptionals.of(of(clazz, annotationClazz));
+    public static <T extends Annotation> Optional<T> optionalOfSupers(final Class<?> clazz,
+	    final Class<T> annotationClazz) {
+	return MyOptionals.of(ofSupers(clazz, annotationClazz));
     }
 
-    public static <T extends Annotation> T of(final Class<?> clazz, final Class<T> annotationClazz) {
-	MyObjects.requireNonNull(clazz, "clazz");
-	MyObjects.requireNonNull(annotationClazz, "annotationClazz");
+    public static <T extends Annotation> T ofSupers(final Class<?> clazz, final Class<T> annotationClazz) {
 	Class<?> nextCurrent = clazz;
 	do {
-	    final T an = nextCurrent.getAnnotation(annotationClazz);
+	    final T an = of(nextCurrent, annotationClazz);
 	    if (an != null)
 		return an;
 	    nextCurrent = nextCurrent.getSuperclass();
 	} while (nextCurrent != null);
 	return null;
+    }
+
+    public static <T extends Annotation> Optional<T> optionalOf(final Class<?> clazz,
+	    final Class<T> annotationClazz) {
+	return MyOptionals.of(of(clazz, annotationClazz));
+    }
+
+    public static <T extends Annotation> T of(final Class<?> clazz, final Class<T> annotationClazz) {
+	return MyObjects.requireNonNull(clazz, "clazz") //
+		.getAnnotation(MyObjects.requireNonNull(annotationClazz, "annotationClazz"));
     }
 
 }
