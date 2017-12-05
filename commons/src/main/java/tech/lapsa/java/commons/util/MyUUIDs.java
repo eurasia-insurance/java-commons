25be55c7284755d658a0605122f3d0b48fffb276
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-05 11:34:07 +0600

    MyUUIDS

---
+++ commons/src/main/java/tech/lapsa/java/commons/util/MyUUIDs.java
@@ -0,0 +1,23 @@
+package tech.lapsa.java.commons.util;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import tech.lapsa.java.commons.function.MyOptionals;
+
+public final class MyUUIDs {
+
+    private MyUUIDs() {
+    }
+
+    public static Optional<UUID> optOf(final String uuid) {
+	return MyOptionals.of(uuid) //
+		.map(s -> {
+		    try {
+			return UUID.fromString(uuid);
+		    } catch (Exception e) {
+			return null;
+		    }
+		});
+    }
+}
