40bb17bf71cc008411c906112965a4eb4c7266f5
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-05-21 19:02:24 +0600

    predicates and trifunction

--- commons/src/main/java/tech/lapsa/java/commons/function/MyFunctions.java
+++ commons/src/main/java/tech/lapsa/java/commons/function/MyFunctions.java
@@ -1,6 +1,8 @@
 package tech.lapsa.java.commons.function;
 
+import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public final class MyFunctions {
 
@@ -14,4 +16,16 @@ public final class MyFunctions {
     public static <T> Consumer<T> voidConsumer() {
 	return (Consumer<T>) VOID_CONSUMER;
     }
+
+    @FunctionalInterface
+    public interface TriFunction<T, U, V, R> {
+
+	R apply(T t, U u, V v);
+
+	default <N> TriFunction<T, U, V, N> andThen(Function<? super R, ? extends N> after) {
+	    Objects.requireNonNull(after);
+	    return (T t, U u, V v) -> after.apply(apply(t, u, v));
+	}
+    }
+
 }
